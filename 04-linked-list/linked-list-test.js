7ebd1aed3477fa97497e6f6f3224e2e16041f082
Ben Harris <ben@harrishills.com>
2018-10-23 13:58:28 -0700

    Adding updates to lab

---
+++ 04-linked-list/linked-list-test.js
@@ -0,0 +1,45 @@
+'use strict';
+
+//! YOUTOUBE: 10/22 at 9:30am
+const LinkedList = require('./linked-list.js');
+
+// Test Suite
+describe('Creation', () => {
+  it('it should construct', () => {
+
+  });
+  it('new list should have null head', () => {
+
+  });
+  it('should have a node as head when constructed with argument', () => {
+
+  });
+});
+
+describe('Insertion', () => {
+  it('should insert node at the head', () => {
+
+  });
+});
+
+describe('includes', () => {
+
+});
+
+describe('print', () => {
+
+});
+
+describe('Linked List Nodes', () => {
+  it('should have null next by default', () => {
+    const input = null;
+    const output = (new Node()).next;
+    expect(input).toBe(output);
+  });
+
+  it('shoudl have next node when passed to constructor', () => {
+    const game1 = new Node('Mass Effect');
+    const game2 = new Node('BioShock', game1);
+    expect().toBe();
+  });
+});
